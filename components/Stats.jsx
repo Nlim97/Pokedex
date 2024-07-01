667805883310093b14b3983a6c11f7d3ba68aba5
Nlim97 <nobellimbu1@gmail.com>
2024-07-01 01:20:08 +0100

    added bar chart for stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,28 +1,39 @@
 import { useEffect, useState } from "react";
+import Chart from "react-google-charts";
 import { useLocation } from "react-router-dom";
 
 function Stats (){
     const location = useLocation()
     const [stats, setStats] = useState([])
+    const options = {
+        title: `${location.state.data.name}'s base stats`,
+        chartArea: { width: "50%" },
+        hAxis: {
+          title: "Base stat",
+          minValue: 0,
+        },
+        vAxis: {
+          title: "Stat name",
+        },
+      };
     useEffect(() =>{
         if(location.state.data.stats){
-            setStats(location.state.data.stats)
+            const statArray = location.state.data.stats.map(statItem => [statItem.stat.name, statItem.base_stat]);
+            setStats([["pokemon", "base stats"], ...statArray])
         }
         
     },[location])
+
+    return (
+        <Chart
+          chartType="BarChart"
+          width="100%"
+          height="400px"
+          data={stats}
+          options={options}
+        />
+      );
     
-    return(
-        <div>
-            {stats.map((stat, index) => {
-                return (
-                    <div key={index}>
-                        <p>{stat.stat.name}</p>
-                        <p>{stat.base_stat}</p> 
-                    </div>
-                )
-            })}
-        </div>
-    )
 }
 
 export default Stats;
